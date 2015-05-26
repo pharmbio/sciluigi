2542910600dd1c1f3e9cb794694140a7d030063d
Samuel Lampa <samuel.lampa@gmail.com>
2015-05-26 20:24:16 +0200

    Continuing on example 2

--- examples/example2_dahlberg.py
+++ examples/example2_dahlberg.py
@@ -1,5 +1,9 @@
 import luigi
 import luigipp
+from subprocess import call
+import time
+import requests
+import sys
 
 # ------------------------------------------------------------------------
 # Task classes
@@ -7,29 +11,57 @@ import luigipp
 
 #Rsync a folder
 class RSyncAFolder(luigipp.LuigiPPTask):
-    pass
+    src_dir_target = luigi.Parameter()
+    dest_dir_path = luigi.Parameter()
 
-#Run a program that takes 10 minutes to run
-class Run10MinuteSleep(luigipp.LuigiPPTask):
-    pass
+    def output(self):
+        # TODO: Let's see if a folder can be used as a target ...
+        return { 'dest_dir' : luigi.LocalTarget(self.dest_dir) }
+
+    def run(self):
+        call('rsync -a {src} {dest}'.format(
+            src = self.get_input('src_dir').path,
+            dest = self.dest_dir))
+
+
+#Run a program that takes 10 minutes (seconds now, for a try) to run
+class Run10MinuteSleep(luigi.LuigiPPTask):
+    upstream_target = luigi.Parameter()
+
+    def output(self):
+        return { 'done_flagfile' : luigi.LocalTarget(self.get_input('upstream_target').path + '.10mintask_done' ) }
+
+    def run(self):
+        time.sleep(10)
+
 
 #Perform a web request
 class DoWebRequest(luigipp.LuigiPPTask):
-    pass
+    upstream_target = luigi.Parameter()
 
-#If it succeeds, continue
+    def output(self):
+        return { 'done_flagfile' : luigi.LocalTarget(self.get_input('upstream_target').path + '.webrequest_done' ) }
 
-#If it fails, die
+    def run(self):
+        resp = requests.get('http://bils.se')
+        if resp.statu_code != 200:
+            raise Exception('Web request failed!')
+            sys.exit()
+        else:
+            with self.output()['done_flagfile'].open('w') as flagfile:
+                flagfile.write('Web Request Task Done!')
+
 
 #Split a file
 class SplitAFile(luigipp.LuigiPPTask):
     pass
 
+
 #Run the same program on both parts of the split
 class DoSomething(luigipp.LuigiPPTask):
     pass
 
+
 #Merge the results of the programs
 class MergeFiles(luigipp.LuigiPPTask):
     pass
-
