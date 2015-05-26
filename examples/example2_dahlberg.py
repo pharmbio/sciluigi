c69238af36d921272f99dd659b3819506bf92bf8
Samuel Lampa <samuel.lampa@gmail.com>
2015-05-26 21:05:44 +0200

    Now luigi CLI works in example 2

--- examples/example2_dahlberg.py
+++ examples/example2_dahlberg.py
@@ -11,7 +11,7 @@ import sys
 
 #Rsync a folder
 class RSyncAFolder(luigipp.LuigiPPTask):
-    src_dir_target = luigi.Parameter()
+    src_dir_path = luigi.Parameter()
     dest_dir_path = luigi.Parameter()
 
     def output(self):
@@ -20,12 +20,12 @@ class RSyncAFolder(luigipp.LuigiPPTask):
 
     def run(self):
         call('rsync -a {src} {dest}'.format(
-            src = self.get_input('src_dir').path,
-            dest = self.dest_dir))
+            src = self.src_dir_path,
+            dest = self.dest_dir_path))
 
 
 #Run a program that takes 10 minutes (seconds now, for a try) to run
-class Run10MinuteSleep(luigi.LuigiPPTask):
+class Run10MinuteSleep(luigipp.LuigiPPTask):
     upstream_target = luigi.Parameter()
 
     def output(self):
@@ -44,7 +44,7 @@ class DoWebRequest(luigipp.LuigiPPTask):
 
     def run(self):
         resp = requests.get('http://bils.se')
-        if resp.statu_code != 200:
+        if resp.status_code != 200:
             raise Exception('Web request failed!')
             sys.exit()
         else:
@@ -53,15 +53,121 @@ class DoWebRequest(luigipp.LuigiPPTask):
 
 
 #Split a file
-class SplitAFile(luigipp.LuigiPPTask):
-    pass
+class ExistingData(luigipp.LuigiPPExternalTask):
+    file_name = luigi.Parameter(default='acgt.txt')
+
+    def output(self):
+        return { 'acgt' : luigi.LocalTarget('data/' + self.file_name) }
+
+class SplitAFile(luigipp.LuigiPPExternalTask):
+    indata_target = luigi.Parameter()
+
+    def output(self):
+        return { 'part1' : luigi.LocalTarget(self.get_input('indata_target').path + '.part1'),
+                 'part2' : luigi.LocalTarget(self.get_input('indata_target').path + '.part2') }
+
+    def run(self):
+        lines_cnt = int(sub.check_output('wc -l {f}'.format(
+            f=self.get_input('indata_target').path),
+        shell=True))
+
+        sub.call('head -n {i} > {part1}'.format(
+            n=self.get_input('indata_target').path,
+            part1=self.output()['part1'].path),
+        shell=True)
+
+        sub.call('tail -n {i} > {part2}'.format(
+            i=self.get_input('indata_target').path,
+            part1=self.output()['part2'].path),
+        shell=True)
 
 
 #Run the same program on both parts of the split
 class DoSomething(luigipp.LuigiPPTask):
-    pass
+    indata_target = luigi.Parameter()
+
+    def output(self):
+        return { 'outdata' : luigi.LocalTarget(self.get_input('indata_target').path + '.something_done' ) }
+
+    def run(self):
+        with self.get_input('indata_target').open() as infile, self.output()['outdata'].open('w') as outfile:
+            for line in infile:
+                outfile.write(line.lower() + '\n')
 
 
 #Merge the results of the programs
 class MergeFiles(luigipp.LuigiPPTask):
-    pass
+    part1_target = luigi.Parameter()
+    part2_target = luigi.Parameter()
+
+    def output(self):
+        return { 'merged' : luigi.LocalTarget(self.get_input('part1_target').path + '_' + self.get_input('part2_target').path + '.merged' ) }
+
+    def run(self):
+        sub.call('cat {f1} {f2} > {out}'.format(
+            f1=self.get_input('part1_target').path,
+            f2=self.get_input('part2_target').path,
+            out=self.output()['merged'].path),
+        shell=True)
+
+# ------------------------------------------------------------------------
+# Workflow class
+# ------------------------------------------------------------------------
+
+class DahlbergTest(luigi.Task):
+
+    task_to_return = luigi.Parameter(default='merge')
+
+    def requires(self):
+
+        tasks = {}
+
+        # Workflow definition goes here!
+
+        #Rsync en mapp
+        tasks['rsync'] = RSyncAFolder(
+                src_dir_path = 'data',
+                dest_dir_path = 'data_copy'
+                )
+
+        #Kor ett program som tar 10 minuter att kora
+        tasks['run10min'] = Run10MinuteSleep(
+                upstream_target = tasks['rsync'].outport('destdir')
+                )
+
+        #Gora en http request ut
+        tasks['webreq'] = DoWebRequest(
+                upstream_target = tasks['run10min'].outport('done_flagfile')
+                )
+
+        tasks['split_indata'] = ExistingData()
+
+        #Splitta en fil
+        tasks['split'] = SplitAFile(
+                indata_target = tasks['split_indata']
+                )
+
+        #Kor samma program pa de tva resultaten
+        tasks['dosth1'] = DoSomething(
+                indata_target = tasks['split_indata'].outport('part1')
+                )
+
+        tasks['dosth2'] = DoSomething(
+                indata_target = tasks['split_indata'].outport('part2')
+                )
+
+        #Merga resultaten
+        tasks['merge'] = MergeFiles(
+                part1_target = tasks['dosth1'].outport('outdata'),
+                part2_target = tasks['dosth2'].outport('outdata')
+                )
+
+        return tasks[self.task_to_return]
+
+
+# ------------------------------------------------------------------------
+# Run this file as a script
+# ------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    luigi.run()
