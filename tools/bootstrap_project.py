360005d65575c4b47b25dc8308e8a5611a00e584
Samuel Lampa <samuel.lampa@gmail.com>
2015-06-20 18:26:34 +0000

    Add comments in bootstrap script

--- tools/bootstrap_project.py
+++ tools/bootstrap_project.py
@@ -1,1 +1,27 @@
 # TODO: Implement!
+
+'''
+We want a folder structure something like this:
+
+|-bin
+|-conf
+|-doc
+| \-paper
+|-experiments
+| \-2000-01-01-example
+|   |-audit
+|   |-bin
+|   |-conf
+|   |-data
+|   |-doc
+|   |-lib
+|   |-log
+|   |-raw
+|   |-results
+|   |-run
+|   \-tmp
+|-lib
+|-raw
+|-results
+\-src
+'''
