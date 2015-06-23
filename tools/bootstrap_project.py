67cfd9b41a9275c85e67b35e628ceceb29c1471f
Samuel Lampa <samuel.lampa@gmail.com>
2015-06-23 13:56:21 +0000

    Implemented print function for folder structure

--- tools/bootstrap_project.py
+++ tools/bootstrap_project.py
@@ -25,3 +25,37 @@ We want a folder structure something like this:
 |-results
 \-src
 '''
+
+projdir_structure = {
+    'bin':None,
+    'conf':None,
+    'doc' : { 'paper': None },
+    'experiments' :
+        { '2000-01-01-example' :
+            {
+                'audit':None,
+                'bin':None,
+                'conf':None,
+                'data':None,
+                'doc':None,
+                'lib':None,
+                'log':None,
+                'raw':None,
+                'results':None,
+                'run':None,
+                'tmp':None
+            }
+        },
+    'lib':None,
+    'raw':None,
+    'results':None,
+    'src':None
+}
+
+def print_dirs(dir_structure, padding, padstep):
+    if type(dir_structure) is dict:
+        for k,v in dir_structure.iteritems():
+            print str(' ' * padding) + k
+            print_dirs(v, padding+padstep, padstep)
+
+print_dirs(projdir_structure, 0, 2)
