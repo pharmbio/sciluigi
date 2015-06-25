83ffe491229f9e665c40fe9e4b8487d14ffd671f
Samuel Lampa <samuel.lampa@gmail.com>
2015-06-25 13:01:45 +0000

    sciluigi -> sl, in example3

--- examples/example3_singleassmt.py
+++ examples/example3_singleassmt.py
@@ -1,26 +1,26 @@
 import luigi
-import sciluigi
+import sciluigi as sl
 import time
 
 # ========================================================================
 
 
-class TestWF(sciluigi.WorkflowTask):
+class TestWF(sl.WorkflowTask):
 
     def workflow(self):
-        t1a = sciluigi.new_task(T1, text='hej_hopp')
+        t1a = sl.new_task(T1, text='hej_hopp')
         print "T1a task id: " + t1a.task_id
         print "T1a hash   : " + str(t1a.__hash__())
 
-        t1b = sciluigi.new_task(T1, text='hopp_hej')
+        t1b = sl.new_task(T1, text='hopp_hej')
         print "T1b task id: " + t1b.task_id
         print "T1b hash   : " + str(t1b.__hash__())
 
-        mrg1 = sciluigi.new_task(Merge)
+        mrg1 = sl.new_task(Merge)
         print "Mrg1 task id: " + mrg1.task_id
         print "Mrg1 hash   : " + str(mrg1.__hash__())
 
-        mrg2 = sciluigi.new_task(Merge)
+        mrg2 = sl.new_task(Merge)
         print "Mrg2 task id: " + mrg2.task_id
         print "Mrg2 hash   : " + str(mrg2.__hash__())
 
@@ -35,7 +35,7 @@ class TestWF(sciluigi.WorkflowTask):
 
 # ========================================================================
 
-class T1(sciluigi.Task):
+class T1(sl.Task):
 
     text = luigi.Parameter()
 
@@ -44,7 +44,7 @@ class T1(sciluigi.Task):
     # ------------------------------------------------
 
     def out_data1(self):
-        return sciluigi.TargetInfo(self, self.text + '.txt') # TODO: Of course make the target spec into an object with "get target" method!
+        return sl.TargetInfo(self, self.text + '.txt') # TODO: Of course make the target spec into an object with "get target" method!
 
     # ------------------------------------------------
 
@@ -54,7 +54,7 @@ class T1(sciluigi.Task):
 
 # ========================================================================
 
-class Merge(sciluigi.Task):
+class Merge(sl.Task):
 
     # ------------------------------------------------
     # I/O
@@ -64,7 +64,7 @@ class Merge(sciluigi.Task):
     in_data2 = None
 
     def out_merged(self): 
-        return sciluigi.TargetInfo(self, self.in_data1.path + '.merged.txt')
+        return sl.TargetInfo(self, self.in_data1.path + '.merged.txt')
 
     # ------------------------------------------------
 
