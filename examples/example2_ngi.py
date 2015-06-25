f503254a396489fcefc62828f8cae570c48132e9
Samuel Lampa <samuel.lampa@gmail.com>
2015-06-25 16:30:19 +0000

    Updated example2_ngi.py to new API

--- examples/example2_ngi.py
+++ examples/example2_ngi.py
@@ -8,178 +8,188 @@ import requests
 import time
 
 # ------------------------------------------------------------------------
+# Workflow class
+# ------------------------------------------------------------------------
+
+class NGITestWF(sl.WorkflowTask):
+    task = luigi.Parameter() # Task to return, chosable on commandline
+
+    def workflow(self):
+        # Rsync a folder
+        rsync = sl.new_task(RSyncAFolder,
+                src_dir_path = 'data',
+                dest_dir_path = 'data_rsynced_copy')
+
+        # Run a program that takes 10 minutes (seconds)
+        run10min = sl.new_task(Run10MinuteSleep)
+        run10min.in_upstream = rsync.out_destdir
+
+        # Do a web request
+        webreq = sl.new_task(DoWebRequest)
+        webreq.in_upstream = run10min.out_doneflag
+
+        # Split a file
+        rawdata = sl.new_task(ExistingData, file_name='acgt.txt')
+
+        split = sl.new_task(SplitAFile)
+        split.in_data = rawdata.out_acgt
+
+        # Run the same task on the two splits
+        dosth1 = sl.new_task(DoSomething)
+        dosth1.in_data = split.out_part1
+
+        dosth2 = sl.new_task(DoSomething)
+        dosth2.in_data = split.out_part2
+
+        # Merge the results
+        merge = sl.new_task(MergeFiles)
+        merge.in_part1 = dosth1.out_data
+        merge.in_part2 = dosth2.out_data
+
+        return locals()[self.task]
+
+# ------------------------------------------------------------------------
 # Task classes
 # ------------------------------------------------------------------------
 
-#Rsync a folder
+# Rsync a folder
 class RSyncAFolder(sl.Task):
+
+    # Params
     src_dir_path = luigi.Parameter()
     dest_dir_path = luigi.Parameter()
 
-    def output(self):
-        # TODO: Let's see if a folder can be used as a target ...
-        return sl.create_file_targets(
-            dest_dir = self.dest_dir_path)
+    # I/O
+    def out_destdir(self):
+        return sl.TargetInfo(self, self.dest_dir_path)
 
+    # Impl
     def run(self):
         call('rsync -a {src}/ {dest}/'.format(
             src = self.src_dir_path,
             dest = self.dest_dir_path),
         shell=True)
 
-
-#Run a program that takes 10 minutes (seconds now, for a try) to run
+# Run a program that takes 10 minutes (seconds now, for a try) to run
 class Run10MinuteSleep(sl.Task):
-    upstream = sl.TargetSpecParameter()
 
-    def output(self):
-        return sl.create_file_targets(
-            doneflag = self.input('upstream').path + '.10mintask_done')
+    # I/O
+    in_upstream = None
 
+    def out_doneflag(self):
+        return sl.TargetInfo(self, self.in_upstream().path + '.10mintask_done')
+
+    # Impl
     def run(self):
         time.sleep(10)
-        with self.output()['doneflag'].open('w') as flagfile:
+        with self.out_doneflag().open('w') as flagfile:
             flagfile.write('Done!')
 
 
-#Perform a web request
+# Perform a web request
 class DoWebRequest(sl.Task):
-    upstream = sl.TargetSpecParameter()
 
-    def output(self):
-        return sl.create_file_targets(
-            doneflag = self.input('upstream').path + '.webrequest_done')
+    # I/O
+    in_upstream = None
 
+    def out_doneflag(self):
+        return sl.TargetInfo(self, self.in_upstream().path + '.webrequest_done')
+
+    # Impl
     def run(self):
         resp = requests.get('http://bils.se')
         if resp.status_code != 200:
             raise Exception('Web request failed!')
             sys.exit()
         else:
-            with self.output()['doneflag'].open('w') as flagfile:
+            with self.out_doneflag().open('w') as flagfile:
                 flagfile.write('Web Request Task Done!')
 
 
-#Split a file
 class ExistingData(sl.ExternalTask):
+
+    # Params
     file_name = luigi.Parameter(default='acgt.txt')
 
-    def output(self):
-        return sl.create_file_targets(
-             acgt = 'data/' + self.file_name)
+    # I/O
+    def out_acgt(self):
+        return sl.TargetInfo(self, 'data/' + self.file_name)
+
 
 class SplitAFile(sl.Task):
-    indata = sl.TargetSpecParameter()
 
-    def output(self):
-        return sl.create_file_targets(
-            part1 = self.input('indata').path + '.part1',
-            part2 = self.input('indata').path + '.part2')
+    # I/O
+    in_data = None
 
+    def out_part1(self):
+        return sl.TargetInfo(self, self.in_data().path + '.part1')
 
+    def out_part2(self):
+        return sl.TargetInfo(self, self.in_data().path + '.part2')
+
+    # Impl
     def run(self):
-        cmd = 'wc -l {f}'.format(f=self.get_path('indata') )
+        cmd = 'wc -l {f}'.format(f=self.in_data().path )
         wc_output = sub.check_output(cmd, shell=True)
         lines_cnt = int(wc_output.split(' ')[0])
         head_cnt = int(math.ceil(lines_cnt / 2))
         tail_cnt = int(math.floor(lines_cnt / 2))
 
         cmd_head = 'head -n {cnt} {i} > {part1}'.format(
-            i=self.get_path('indata'),
+            i=self.in_data().path,
             cnt=head_cnt,
-            part1=self.output()['part1'].path)
+            part1=self.out_part1().path)
         print("COMMAND: " + cmd_head)
         sub.call(cmd_head, shell=True)
 
         sub.call('tail -n {cnt} {i} {cnt} > {part2}'.format(
-            i=self.get_path('indata'),
+            i=self.in_data().path,
             cnt=tail_cnt,
-            part2=self.output()['part2'].path),
+            part2=self.out_part2().path),
         shell=True)
 
 
-#Run the same program on both parts of the split
 class DoSomething(sl.Task):
-    indata = sl.TargetSpecParameter()
+    '''
+    Run the same program on both parts of the split
+    '''
 
-    def output(self):
-        return sl.create_file_targets(
-            outdata = self.get_path('indata') + '.something_done')
+    # I/O
+    in_data = None
 
+    def out_data(self):
+        return sl.TargetInfo(self, self.in_data().path + '.something_done')
+
+    # Impl
     def run(self):
-        with self.input('indata').open() as infile, self.output()['outdata'].open('w') as outfile:
+        with self.in_data().open() as infile, self.out_data().open('w') as outfile:
             for line in infile:
                 outfile.write(line.lower() + '\n')
 
 
-#Merge the results of the programs
 class MergeFiles(sl.Task):
-    part1 = sl.TargetSpecParameter()
-    part2 = sl.TargetSpecParameter()
+    '''
+    Merge the results of the programs
+    '''
 
-    def output(self):
-        return sl.create_file_targets(
-            merged = self.input('part1').path + '.merged'
-        )
+    # I/O
+    in_part1 = None
+    in_part2 = None
 
+    def out_merged(self):
+        return sl.TargetInfo(self, self.in_part1().path + '.merged')
+
+    # Impl
     def run(self):
         sub.call('cat {f1} {f2} > {out}'.format(
-            f1=self.input('part1').path,
-            f2=self.input('part2').path,
-            out=self.output()['merged'].path),
+            f1=self.in_part1().path,
+            f2=self.in_part2().path,
+            out=self.out_merged().path),
         shell=True)
 
 # ------------------------------------------------------------------------
-# Workflow class
-# ------------------------------------------------------------------------
-
-class DahlbergTest(sl.WorkflowTask):
-
-    task = luigi.Parameter()
-
-    def requires(self):
-
-        tasks = {}
-
-        # Workflow definition goes here!
-
-        # Rsync a folder
-        tasks['rsync'] = RSyncAFolder(
-                src_dir_path = 'data',
-                dest_dir_path = 'data_rsynced_copy')
-
-        # Run a program that takes 10 minutes (seconds)
-        tasks['run10min'] = Run10MinuteSleep(
-                upstream = tasks['rsync'].outspec('dest_dir'))
-
-        # Do a web request
-        tasks['webreq'] = DoWebRequest(
-                upstream = tasks['run10min'].outspec('doneflag'))
-
-        tasks['rawdata'] = ExistingData()
-
-        # Split a file
-        tasks['split'] = SplitAFile(
-                indata = tasks['rawdata'].outspec('acgt'))
-
-        # Run the same task on the two splits
-        tasks['dosth1'] = DoSomething(
-                indata = tasks['split'].outspec('part1'))
-
-        tasks['dosth2'] = DoSomething(
-                indata = tasks['split'].outspec('part2'))
-
-        # Merge the results
-        tasks['merge'] = MergeFiles(
-                part1 = tasks['dosth1'].outspec('outdata'),
-                part2 = tasks['dosth2'].outspec('outdata'))
-
-        return tasks[self.task]
-
-
-# ------------------------------------------------------------------------
-# Run this file as a script
+# Run as script
 # ------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    luigi.run()
+    sl.run_locally()
