ac79d900eae6d1c60d999c85685e416d171d1e91
Samuel Lampa <samuel.lampa@gmail.com>
2015-08-25 23:37:23 +0200

    Bugfix and add ensuredir() in util

--- sciluigi/util.py
+++ sciluigi/util.py
@@ -1,10 +1,15 @@
 import time
+import os
 
 def timestamp(datefmt='%Y-%m-%d, %H:%M:%S'):
     return time.strftime(datefmt, time.localtime())
 
-def timepath(sef='_'):
+def timepath(sep='_'):
     return timestamp('%Y%m%d{sep}%H%M%S'.format(sep=sep))
 
 def timelog():
     return timestamp('[%Y-%m-%d %H:%M:%S]')
+
+def ensuredir(dirpath):
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
