daf86b562301095f44ea6d940c8ff52aedb34b9c
Samuel Lampa <samuel.lampa@gmail.com>
2015-08-25 23:37:43 +0200

    Setup logging in interface

--- sciluigi/interface.py
+++ sciluigi/interface.py
@@ -1,7 +1,40 @@
 import luigi
+import logging
+import util
 
 def run(*args, **kwargs):
     luigi.run(*args, **kwargs) 
 
 def run_locally():
     luigi.run(local_scheduler=True)
+
+def setup_logging():
+    # Formatter
+    log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s','%Y-%m-%d %H:%M:%S')
+
+    # Handlers
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(log_formatter)
+    stream_handler.setLevel(logging.INFO)
+
+    util.ensuredir('log')
+    file_handler = logging.FileHandler('log/sciluigi_run_%s.log' % util.timepath())
+    file_handler.setFormatter(log_formatter)
+    file_handler.setLevel(logging.DEBUG)
+
+    # SciLuigi logger
+    sciluigi_logger = logging.getLogger('sciluigi-interface')
+    sciluigi_logger.addHandler(stream_handler)
+    sciluigi_logger.addHandler(file_handler)
+    sciluigi_logger.setLevel(logging.DEBUG)
+
+    # Luigi logger
+    luigi_logger = logging.getLogger('luigi-interface')
+    luigi_logger.addHandler(stream_handler)
+    luigi_logger.addHandler(file_handler)
+    luigi_logger.setLevel(logging.DEBUG)
+
+    # Mark luigi logging setup as done
+    luigi.interface.setup_interface_logging.has_run = True
+
+setup_logging()
