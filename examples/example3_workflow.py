c6011e3ba8e47cb2aae93baa15aafa0d03cde4fc
Samuel Lampa <samuel.lampa@gmail.com>
2015-08-29 01:15:56 +0200

    Updating example3 to use latest SciLuigi features

--- examples/example3_workflow.py
+++ examples/example3_workflow.py
@@ -1,7 +1,13 @@
+import logging
 import luigi
 import sciluigi as sl
 import time
 from example3_components import T1, Merge
+
+# ========================================================================
+
+log = logging.getLogger('sciluigi-interface')
+
 # ========================================================================
 
 class TestWF(sl.WorkflowTask):
@@ -9,11 +15,11 @@ class TestWF(sl.WorkflowTask):
     task = luigi.Parameter()
 
     def workflow(self):
-        t1a = sl.new_task('t1a', T1, self, text='hej_hopp')
-        t1b = sl.new_task('t1b', T1, self, text='hopp_hej')
+        t1a = self.new_task('t1a', T1, text='hej_hopp')
+        t1b = self.new_task('t1b', T1, text='hopp_hej')
 
-        mrg1 = sl.new_task('mrg1', Merge, self)
-        mrg2 = sl.new_task('mrg2', Merge, self)
+        mrg1 = self.new_task('mrg1', Merge)
+        mrg2 = self.new_task('mrg2', Merge)
 
         # Workflow definition
         mrg1.in_data1 = t1a.out_data1
@@ -24,36 +30,12 @@ class TestWF(sl.WorkflowTask):
 
         for name, instance in locals().iteritems():
             if issubclass(type(instance), sl.Task):
-                print '{n} task id: {taskid}\n{n} hash   : {h}'.format(
+                log.info('{n}, task id: {taskid}\n{n}, hash: {h}'.format(
                         n = name,
                         taskid = instance.task_id,
-                        h = instance.__hash__())
+                        h = instance.__hash__()))
 
         return locals()[self.task]
 
-slurm_conf = {
-               'default' :    {
-                                 't1a'  : sl.SlurmInfo('core', 1, '1:00:00'),
-                                 't1b'  : sl.SlurmInfo('core', 1, '1:00:00'),
-                                 'mrg1' : sl.SlurmInfo('core', 1, '1:00:00'),
-                                 'mrg2' : sl.SlurmInfo('core', 1, '2:00:00'),
-                              },
-               # ----------------------------------------------------------------
-               'medium' :     {
-                                 't1a'  : sl.SlurmInfo('core', 1, '1:00:00'),
-                                 't1b'  : sl.SlurmInfo('core', 1, '1:00:00'),
-                                 'mrg1' : sl.SlurmInfo('core', 1, '1:00:00'),
-                                 'mrg2' : sl.SlurmInfo('core', 1, '2:00:00'),
-                              },
-               # ----------------------------------------------------------------
-               'production' : {
-                                 't1a'  : sl.SlurmInfo('core', 1, '1:00:00'),
-                                 't1b'  : sl.SlurmInfo('core', 1, '1:00:00'),
-                                 'mrg1' : sl.SlurmInfo('core', 1, '1:00:00'),
-                                 'mrg2' : sl.SlurmInfo('core', 1, '2:00:00'),
-                              },
-             }
-
 if __name__ == '__main__':
-    sl.run_locally()
-
+    sl.run_local(main_task_cls=TestWF, cmdline_args=['--task=mrg2'])
