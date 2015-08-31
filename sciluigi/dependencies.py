d78e87520585c5f96ed03143395d340cf38b4f95
Samuel Lampa <samuel.lampa@gmail.com>
2015-08-31 18:02:19 +0200

    Greatly simplifying _output_targets() with a recursive inner function _parse_outputitems()

--- sciluigi/dependencies.py
+++ sciluigi/dependencies.py
@@ -77,28 +77,35 @@ class DependencyHelpers():
     # --------------------------------------------------------
 
     def output(self):
+        '''
+        Extract output targets from the TargetInfo objects
+        or functions returning those (or lists of both the earlier)
+        for use in luigi's output() method.
+        '''
         return self._output_targets()
 
     def _output_targets(self):
-        outputs = []
+        output_targets = []
         for attrname in dir(self):
             attrval = getattr(self, attrname)
             if attrname[0:4] == 'out_':
-                # Function returning list of TargetInfos
-                if callable(attrval):
-                    val = attrval()
-                    if isinstance(val, TargetInfo):
-                        outputs.append(val.target)
-                    elif isinstance(val, list):
-                        for item in val:
-                            if callable(item):
-                                outputs.append(item().target)
-                            elif isinstance(item, TargetInfo):
-                                outputs.append(item.target)
-                            else:
-                                raise Exception('Item in list returned by %s neither function nor TargetInfo!' % attrname)
-                else:
-                    raise Exception('Attribute %s is not callable!' % attrname)
+                output_targets = self._parse_outputitem(attrval, output_targets)
 
+        return output_targets
 
-        return outputs
+    def _parse_outputitem(self, val, targets):
+        '''
+        Recursively loop through lists of TargetInfos, or
+        callables returning TargetInfos, or lists of ...
+        (repeat recursively) ... and return all targets.
+        '''
+        if callable(val):
+            val = val()
+        if isinstance(val, TargetInfo):
+            targets.append(val.target)
+        elif isinstance(val, list):
+            for valitem in val:
+                targets = self._parse_outputitem(valitem, targets)
+        else:
+            raise Exception('Input item is neither callable, TargetInfo, nor list: %s' % val)
+        return targets
