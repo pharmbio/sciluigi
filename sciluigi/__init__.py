85beb3067bba562dc2fdf1b1883d683cce6f90f7
Samuel Lampa <samuel.lampa@gmail.com>
2015-08-31 22:14:26 +0200

    Implementing issue #6: Import instead of map to new vars in __init__.py

--- sciluigi/__init__.py
+++ sciluigi/__init__.py
@@ -1,47 +1,36 @@
 from sciluigi import audit
+from sciluigi.audit import AuditTrailHelpers
+
 from sciluigi import dependencies
+from sciluigi.dependencies import TargetInfo
+from sciluigi.dependencies import TargetInfoParameter
+from sciluigi.dependencies import DependencyHelpers
+
 from sciluigi import interface
+from sciluigi.interface import run
+from sciluigi.interface import run_local
+from sciluigi.interface import logfmt_stream
+from sciluigi.interface import logfmt_luigi
+from sciluigi.interface import logfmt_sciluigi
+from sciluigi.interface import datefmt
+
 from sciluigi import parameter
+from sciluigi.parameter import Parameter
+
 from sciluigi import slurm
+from sciluigi.slurm import SlurmInfo
+from sciluigi.slurm import SlurmTask
+from sciluigi.slurm import SlurmHelpers
+from sciluigi.slurm import RUNMODE_LOCAL
+from sciluigi.slurm import RUNMODE_HPC
+from sciluigi.slurm import RUNMODE_MPI
+
 from sciluigi import task
+from sciluigi.task import new_task
+from sciluigi.task import Task
+from sciluigi.task import ExternalTask
+from sciluigi.task import WorkflowTask
+
 from sciluigi import util
-#from sciluigi import slurm
-
-# interface
-run = interface.run
-run_local = interface.run_local
-
-# Format strings
-logfmt_stream = interface.logfmt_stream
-logfmt_luigi  = interface.logfmt_luigi
-logfmt_sciluigi  = interface.logfmt_sciluigi
-datefmt = interface.datefmt
-
-# dependencies module
-TargetInfo = dependencies.TargetInfo
-TargetInfoParameter = dependencies.TargetInfoParameter
-DependencyHelpers = dependencies.DependencyHelpers
-
-# parameter module
-Parameter = parameter.Parameter
-
-# task module
-new_task = task.new_task
-Task = task.Task
-ExternalTask = task.ExternalTask
-WorkflowTask = task.WorkflowTask
-
-# slurm module
-SlurmInfo = slurm.SlurmInfo
-SlurmTask = slurm.SlurmTask
-SlurmHelpers = slurm.SlurmHelpers
-RUNMODE_LOCAL = slurm.RUNMODE_LOCAL
-RUNMODE_HPC = slurm.RUNMODE_HPC
-RUNMODE_MPI = slurm.RUNMODE_MPI
-
-# audit module
-AuditTrailHelpers = audit.AuditTrailHelpers
-
-# Util module
-timestamp = util.timestamp
-timepath = util.timepath
+from sciluigi.util import timestamp
+from sciluigi.util import timepath
