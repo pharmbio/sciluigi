a2c6592e3920d8b15c2970d453ff4e64e8669dd2
Samuel Lampa <samuel.lampa@gmail.com>
2015-08-31 16:30:20 +0200

    Add tests for lists of inputs/outputs

--- test/dependencies_test.py
+++ test/dependencies_test.py
@@ -2,73 +2,78 @@ import logging
 import luigi
 import sciluigi as sl
 import os
+import time
+import unittest
 
 TESTFILE_PATH = '/tmp/test.out'
 
 log = logging.getLogger('sciluigi-interface')
 log.setLevel(logging.WARNING)
 
-class TestTask(sl.Task):
-
-    def out_data(self):
-        return sl.TargetInfo(self, TESTFILE_PATH)
-
-    def run(self):
-        with self.out_data().open('w') as outfile:
-            outfile.write('File written by luigi\n')
-
-class TestRunTask():
-
-    def setup(self):
-        wf = sl.WorkflowTask()
-        self.t = sl.new_task('testtask', TestTask, wf)
-
-    def teardown(self):
-        self.t = None
-        os.remove(TESTFILE_PATH)
-
-    def test_run(self):
-        # Run a task with a luigi worker
-        w = luigi.worker.Worker()
-        w.add(self.t)
-        w.run()
-        w.stop()
-
-        assert os.path.isfile(TESTFILE_PATH)
-
-class TestMultiOut():
-    wf = None
-    def setup(self):
-        self.wf = MultiOutWf()
-    def teardown(self):
-        del(self.wf)
-    def test_run(self):
-        self.wf.run()
-
-class MultiOutWf(sl.WorkflowTask):
+class MultiInOutWf(sl.WorkflowTask):
     def workflow(self):
-        m_out = self.new_task('mout', MultiOutTask)
-        m_in = self.new_task('min', MultiInTask)
-        m_in.in_multi = m_out.out_multi
-        return m_in
+        mo = self.new_task('mout', MultiOutTask)
+        mi = self.new_task('min', MultiInTask)
+        mi.in_multi = mo.out_multi
+        return mi
 
 class MultiOutTask(sl.Task):
     def out_multi(self):
-        return [sl.TargetInfo(self, 'out_%d.txt' % i) for i in xrange(10)]
+        return [sl.TargetInfo(self, '/tmp/out_%d.txt' % i) for i in xrange(10)]
     def run(self):
-        for otgtg in out_multi():
+        for otgt in self.out_multi():
             with otgt.open('w') as ofile:
                 ofile.write('hej')
 
 class MultiInTask(sl.Task):
     in_multi = None
     def out_multi(self):
-        return [sl.TargetInfo(self, itgt.path + '.daa') for itgt in self.in_multi()]
+        return [sl.TargetInfo(self, itgt.path + '.daa.txt') for itgt in self.in_multi()]
     def run(self):
-        for itgt, otgt in zip(in_multi(), out_multi()):
+        for itgt, otgt in zip(self.in_multi(), self.out_multi()):
             with itgt.open() as ifile:
                 with otgt.open('w') as ofile:
                     ofile.write(ifile.read() + ' daa')
 
-#if __name__ == '__main__':
-#    sl.run_local(main_task_cls=MultiOutWf)
+class TestMultiInOutWorkflow(unittest.TestCase):
+    def setUp(self):
+        self.w = luigi.worker.Worker()
+
+    def test_methods(self):
+        wf = sl.WorkflowTask()
+        tout = wf.new_task('tout', MultiOutTask)
+        tin = wf.new_task('tout', MultiInTask)
+
+        # Assert outputs returns luigi targets, or list of luigi targets
+        outs = tout.output()
+        self.assertIsInstance(outs, list)
+        for out in outs:
+            self.assertIsInstance(out, luigi.Target)
+
+        reqs = tin.requires()
+        self.assertIsInstance(reqs, list)
+        for req in reqs:
+            self.assertIsInstance(req, luigi.Task)
+
+    def test_workflow(self):
+        wf = MultiInOutWf()
+        self.w.add(wf)
+        self.w.run()
+
+        # Assert outputs exists
+        for p in ['/tmp/out_%d.txt' % i for i in xrange(10)]:
+            self.assertTrue(os.path.exists(p))
+        for p in ['/tmp/out_%d.txt.daa.txt' % i for i in xrange(10)]:
+            self.assertTrue(os.path.exists(p))
+
+        # Remove
+        for p in ['/tmp/out_%d.txt' % i for i in xrange(10)]:
+            os.remove(p)
+        for p in ['/tmp/out_%d.txt.daa.txt' % i for i in xrange(10)]:
+            os.remove(p)
+
+    def tearDown(self):
+        self.w.stop()
+
+if __name__ == '__main__':
+    unittest.main()
