791d3a02f103a440231f19acbabeff67682c2cc9
Samuel Lampa <samuel.lampa@gmail.com>
2015-10-05 13:38:17 +0200

    Fixed some pylint warnings in audit module

--- sciluigi/audit.py
+++ sciluigi/audit.py
@@ -1,10 +1,12 @@
+'''
+This module contains functionality for the audit-trail logging functionality
+'''
+
 import logging
 import luigi
 import os
 import random
-import string
 import time
-from collections import namedtuple
 
 # ==============================================================================
 
@@ -12,14 +14,20 @@ log = logging.getLogger('sciluigi-interface')
 
 # ==============================================================================
 
-class AuditTrailHelpers():
+class AuditTrailHelpers(object):
     '''
     Mixin for luigi.Task:s, with functionality for writing audit logs of running tasks
     '''
     def add_auditinfo(self, infotype, infoval):
+        '''
+        Alias to _add_auditinfo(), that can be overridden.
+        '''
         return self._add_auditinfo(self.instance_name, infotype, infoval)
 
     def _add_auditinfo(self, instance_name, infotype, infoval):
+        '''
+        Save audit information in a designated file, specific for this task.
+        '''
         dirpath = self.workflow_task.get_auditdirpath()
         if not os.path.isdir(dirpath):
             time.sleep(random.random())
@@ -34,6 +42,9 @@ class AuditTrailHelpers():
             afile.write('%s: %s\n' % (infotype, infoval))
 
     def get_instance_name(self):
+        '''
+        Return the luigi instance_name
+        '''
         instance_name = None
         if self.instance_name is not None:
             instance_name = self.instance_name
@@ -41,22 +52,25 @@ class AuditTrailHelpers():
             instance_name = self.task_id
         return instance_name
 
-    def get_timestamp(self):
-        return timelog()
-
     @luigi.Task.event_handler(luigi.Event.START)
     def save_start_time(self):
+        '''
+        Log start of execution of task.
+        '''
         if hasattr(self, 'workflow_task') and self.workflow_task is not None:
             msg = 'Task {task} started'.format(
-                    task = self.get_instance_name())
+                task=self.get_instance_name())
             log.info(msg)
 
     @luigi.Task.event_handler(luigi.Event.PROCESSING_TIME)
     def save_end_time(self, task_exectime_sec):
+        '''
+        Log end of execution of task, with execution time.
+        '''
         if hasattr(self, 'workflow_task') and self.workflow_task is not None:
             msg = 'Task {task} finished after {proctime:.3f}s'.format(
-                    task = self.get_instance_name(),
-                    proctime = task_exectime_sec)
+                task=self.get_instance_name(),
+                proctime=task_exectime_sec)
             log.info(msg)
             self.add_auditinfo('task_exectime_sec', '%.3f' % task_exectime_sec)
             for paramname, paramval in self.param_kwargs.iteritems():
