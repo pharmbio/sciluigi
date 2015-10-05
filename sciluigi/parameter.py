23940382ffdfee425983154aba97daad3a320c5e
Samuel Lampa <samuel.lampa@gmail.com>
2015-10-05 14:32:47 +0200

    Fixing pylint warnings in parameter module

--- sciluigi/parameter.py
+++ sciluigi/parameter.py
@@ -1,4 +1,12 @@
+'''
+This module contains a sciluigi subclass of luigi's Parameter, where
+custom functionality might be added in the future.
+'''
+
 import luigi
 
 class Parameter(luigi.Parameter):
+    '''
+    Subclass of luigi's Parameter, where custom functionality might be added in the future.
+    '''
     pass
