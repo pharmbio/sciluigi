9c00e2d2762924ed9b683a478f299f6cf90c38c2
Samuel Lampa <samuel.lampa@gmail.com>
2015-10-05 15:19:41 +0200

    Fixing some pylint warnings etc in task module

--- sciluigi/task.py
+++ sciluigi/task.py
@@ -1,11 +1,12 @@
-from collections import namedtuple
+'''
+This module contains sciluigi's subclasses of luigi's Task class.
+'''
+
 import luigi
 import logging
-import random
-import string
 import subprocess as sub
+import os
 import time
-from sciluigi.util import *
 import sciluigi.audit
 import sciluigi.interface
 import sciluigi.dependencies
@@ -15,31 +16,42 @@ log = logging.getLogger('sciluigi-interface')
 
 # ==============================================================================
 
-def new_task(name, cls, workflow_task, **kwargs): # TODO: Raise exceptions if params not of right type
+def new_task(name, cls, workflow_task, **kwargs):
+    '''
+    Instantiate a new task. Not supposed to be used by the end-user
+    (use WorkflowTask.new_task() instead).
+    '''
     slurminfo = None
-    for k, v in [(k,v) for k,v in kwargs.iteritems()]:
+    for key, val in [(key, val) for key, val in kwargs.iteritems()]:
         # Handle non-string keys
-        if not isinstance(k, basestring):
-            raise Exception("Key in kwargs to new_task is not string. Must be string: %s" % k)
+        if not isinstance(key, basestring):
+            raise Exception("Key in kwargs to new_task is not string. Must be string: %s" % key)
         # Handle non-string values
-        if isinstance(v, sciluigi.slurm.SlurmInfo):
-            slurminfo = v
-            kwargs[k] = v
-        elif not isinstance(v, basestring):
-            kwargs[k] = str(v) # Force conversion into string
+        if isinstance(val, sciluigi.slurm.SlurmInfo):
+            slurminfo = val
+            kwargs[key] = val
+        elif not isinstance(val, basestring):
+            kwargs[key] = str(val) # Force conversion into string
     kwargs['instance_name'] = name
     kwargs['workflow_task'] = workflow_task
     kwargs['slurminfo'] = slurminfo
-    t = cls.from_str_params(kwargs)
+    newtask = cls.from_str_params(kwargs)
     if slurminfo is not None:
-        t.slurminfo = slurminfo
-    return t
+        newtask.slurminfo = slurminfo
+    return newtask
 
 class Task(sciluigi.audit.AuditTrailHelpers, sciluigi.dependencies.DependencyHelpers, luigi.Task):
+    '''
+    SciLuigi Task, implementing SciLuigi specific functionality for dependency resolution
+    and audit trail logging.
+    '''
     workflow_task = luigi.Parameter()
     instance_name = luigi.Parameter()
 
     def ex_local(self, command):
+        '''
+        Execute command locally (not through resource manager).
+        '''
         # If list, convert to string
         if isinstance(command, list):
             command = sub.list2cmdline(command)
@@ -50,7 +62,7 @@ class Task(sciluigi.audit.AuditTrailHelpers, sciluigi.dependencies.DependencyHel
         retcode = proc.returncode
 
         if len(stderr) > 0:
-            log.debug('Stderr from command: %s' % stderr)
+            log.debug('Stderr from command: %s', stderr)
 
         if retcode != 0:
             errmsg = ('Command failed (retcode {ret}): {cmd}\n'
@@ -67,19 +79,31 @@ class Task(sciluigi.audit.AuditTrailHelpers, sciluigi.dependencies.DependencyHel
 
     def ex(self, command):
         '''
-        This is a short-hand function, to be overridden e.g. if supporting execution via SLURM
+        Execute command. This is a short-hand function, to be overridden e.g. if supporting
+        execution via SLURM
         '''
         return self.ex_local(command)
 
 # ==============================================================================
 
-class ExternalTask(sciluigi.audit.AuditTrailHelpers, sciluigi.dependencies.DependencyHelpers, luigi.ExternalTask):
+class ExternalTask(
+        sciluigi.audit.AuditTrailHelpers,
+        sciluigi.dependencies.DependencyHelpers,
+        luigi.ExternalTask):
+    '''
+    SviLuigi specific implementation of luigi.ExternalTask, representing existing
+    files.
+    '''
     workflow_task = luigi.Parameter()
     instance_name = luigi.Parameter()
 
 # ================================================================================
 
 class WorkflowTask(sciluigi.audit.AuditTrailHelpers, luigi.Task):
+    '''
+    SciLuigi-specific task, that has a method for implementing a (dynamic) workflow
+    definition (workflow()).
+    '''
 
     instance_name = luigi.Parameter(default='sciluigi_workflow')
 
@@ -91,39 +115,64 @@ class WorkflowTask(sciluigi.audit.AuditTrailHelpers, luigi.Task):
     _hasaddedhandler = False
 
     def _ensure_timestamp(self):
+        '''
+        Make sure that there is a time stamp for when the workflow started.
+        '''
         if self._wfstart == '':
             self._wfstart = time.strftime('%Y%m%d_%H%M%S', time.localtime())
 
     def get_wflogpath(self):
+        '''
+        Get the path to the workflow-speicfic log file.
+        '''
         if self._wflogpath == '':
             self._ensure_timestamp()
             clsname = self.__class__.__name__.lower()
             logpath = 'log/workflow_' + clsname + '_started_{t}.log'.format(t=self._wfstart)
-            log.info('Logging to %s' % logpath)
+            log.info('Logging to %s', logpath)
             self._wflogpath = logpath
         return self._wflogpath
 
     def get_auditdirpath(self):
+        '''
+        Get the path to the workflow-speicfic audit trail directory.
+        '''
         self._ensure_timestamp()
         clsname = self.__class__.__name__.lower()
         audit_dirpath = 'audit/.audit_%s_%s' % (clsname, self._wfstart)
         return audit_dirpath
 
     def get_auditlogpath(self):
+        '''
+        Get the path to the workflow-speicfic audit trail file.
+        '''
         self._ensure_timestamp()
         clsname = self.__class__.__name__.lower()
         audit_dirpath = 'audit/workflow_%s_started_%s.audit' % (clsname, self._wfstart)
         return audit_dirpath
 
     def add_auditinfo(self, infotype, infolog):
+        '''
+        Add audit information to the audit log.
+        '''
         return self._add_auditinfo(self.__class__.__name__.lower(), infotype, infolog)
 
     def workflow(self):
-        raise WorkflowNotImplementedException('workflow() method is not implemented, for ' + str(self))
+        '''
+        SciLuigi API methoed. Implement your workflow here, and return the last task(s)
+        of the dependency graph.
+        '''
+        raise WorkflowNotImplementedException(
+                'workflow() method is not implemented, for ' + str(self))
 
     def requires(self):
+        '''
+        Implementation of Luigi API method.
+        '''
         if not self._hasaddedhandler:
-            wflog_formatter = logging.Formatter(sciluigi.interface.LOGFMT_STREAM, sciluigi.interface.DATEFMT)
+            wflog_formatter = logging.Formatter(
+                    sciluigi.interface.LOGFMT_STREAM,
+                    sciluigi.interface.DATEFMT)
             wflog_file_handler = logging.FileHandler(self.output()['log'].path)
             wflog_file_handler.setLevel(logging.INFO)
             wflog_file_handler.setFormatter(wflog_formatter)
@@ -134,22 +183,30 @@ class WorkflowTask(sciluigi.audit.AuditTrailHelpers, luigi.Task):
         clsname = self.__class__.__name__
         if not self._hasloggedstart:
             log.info('-'*80)
-            log.info('SciLuigi: %s Workflow Started' % clsname)
+            log.info('SciLuigi: %s Workflow Started', clsname)
             log.info('-'*80)
             self._hasloggedstart = True
         workflow_output = self.workflow()
         if workflow_output is None:
             clsname = self.__class__.__name__
-            raise Exception('Nothing returned from workflow() method in the %s Workflow task. Forgot to add a return statement at the end?' % clsname)
+            raise Exception(('Nothing returned from workflow() method in the %s Workflow task. '
+                             'Forgot to add a return statement at the end?') % clsname)
         return workflow_output
 
     def output(self):
+        '''
+        Implementation of Luigi API method
+        '''
         return {'log': luigi.LocalTarget(self.get_wflogpath()),
                 'audit': luigi.LocalTarget(self.get_auditlogpath())}
 
     def run(self):
+        '''
+        Implementation of Luigi API method
+        '''
         if self.output()['audit'].exists():
-            errmsg = 'Audit file already exists, when trying to create it: %s' % self.output()['audit'].path
+            errmsg = ('Audit file already exists, '
+                      'when trying to create it: %s') % self.output()['audit'].path
             log.error(errmsg)
             raise Exception(errmsg)
         else:
@@ -161,16 +218,22 @@ class WorkflowTask(sciluigi.audit.AuditTrailHelpers, luigi.Task):
         clsname = self.__class__.__name__
         if not self._hasloggedfinish:
             log.info('-'*80)
-            log.info('SciLuigi: %s Workflow Finished' % clsname)
+            log.info('SciLuigi: %s Workflow Finished', clsname)
             log.info('-'*80)
             self._hasloggedfinish = True
 
     def new_task(self, instance_name, cls, **kwargs):
-        t = new_task(instance_name, cls, self, **kwargs)
-        self._tasks[instance_name] = t
-        return t
+        '''
+        Create new task instance, and link it to the current workflow.
+        '''
+        newtask = new_task(instance_name, cls, self, **kwargs)
+        self._tasks[instance_name] = newtask
+        return newtask
 
 # ================================================================================
 
 class WorkflowNotImplementedException(Exception):
+    '''
+    Exception to throw if the workflow() SciLuigi API method is not implemented.
+    '''
     pass
