9195ed957a6926df973b338e75d2e520da3760d6
Samuel Lampa <samuel.lampa@gmail.com>
2015-10-05 15:22:15 +0200

    Fixed pylint warnings in util module

--- sciluigi/util.py
+++ sciluigi/util.py
@@ -1,23 +1,43 @@
+'''
+This module contains utility methods that are used in various places across the
+sciluigi library
+'''
+
 import csv
 import os
 import time
 
 def timestamp(datefmt='%Y-%m-%d, %H:%M:%S'):
+    '''
+    Create timestamp as a formatted string.
+    '''
     return time.strftime(datefmt, time.localtime())
 
 def timepath(sep='_'):
+    '''
+    Create timestmap, formatted for use in file names.
+    '''
     return timestamp('%Y%m%d{sep}%H%M%S'.format(sep=sep))
 
 def timelog():
+    '''
+    Create time stamp for use in log files.
+    '''
     return timestamp('[%Y-%m-%d %H:%M:%S]')
 
 def ensuredir(dirpath):
+    '''
+    Ensure directory exists.
+    '''
     if not os.path.exists(dirpath):
         os.makedirs(dirpath)
 
 RECORDFILE_DELIMITER = ':'
 
 def recordfile_to_dict(filehandle):
+    '''
+    Convert a record file to a dictionary.
+    '''
     csvrd = csv.reader(filehandle, delimiter=RECORDFILE_DELIMITER, skipinitialspace=True)
     records = {}
     for row in csvrd:
@@ -25,8 +45,11 @@ def recordfile_to_dict(filehandle):
     return records
 
 def dict_to_recordfile(filehandle, records):
+    '''
+    Convert a dictionary to a recordfile.
+    '''
     csvwt = csv.writer(filehandle, delimiter=RECORDFILE_DELIMITER, skipinitialspace=True)
     rows = []
-    for k, v in records.iteritems():
-       rows.append([k, v])
+    for key, val in records.iteritems():
+        rows.append([key, val])
     csvwt.writerows(rows)
