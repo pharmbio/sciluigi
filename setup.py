7c7b8a8688de576b144336cb27b812865d91506a
Samuel Lampa <samuel.lampa@gmail.com>
2017-09-21 16:39:44 +0200

    Bump version number to 0.9.6b7

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('README.rst') as fobj:
 
 setup(
     name='sciluigi',
-    version='0.9.5b6',
+    version='0.9.6b7',
     description='Helper library for writing dynamic, flexible workflows in luigi',
     long_description=long_description,
     author='Samuel Lampa',
