1018119cf3b6952565861fb2340e865d6ffb5dc3
Samuel Lampa <samuel.lampa@rilnet.com>
2020-05-27 16:08:14 +0200

    Improve module setup

--- sciluigi/__init__.py
+++ sciluigi/__init__.py
@@ -5,14 +5,6 @@ Spotify's Luigi workflow system that aims to make writing scientific workflows
 flexible and modular.
 '''
 
-from sciluigi import audit
-from sciluigi.audit import AuditTrailHelpers
-
-from sciluigi import dependencies
-from sciluigi.dependencies import TargetInfo
-from sciluigi.dependencies import S3TargetInfo
-from sciluigi.dependencies import DependencyHelpers
-
 from sciluigi import interface
 from sciluigi.interface import run
 from sciluigi.interface import run_local
@@ -20,6 +12,14 @@ from sciluigi.interface import LOGFMT_STREAM
 from sciluigi.interface import LOGFMT_LUIGI
 from sciluigi.interface import LOGFMT_SCILUIGI
 from sciluigi.interface import DATEFMT
+
+from sciluigi import audit
+from sciluigi.audit import AuditTrailHelpers
+
+from sciluigi import dependencies
+from sciluigi.dependencies import TargetInfo
+from sciluigi.dependencies import S3TargetInfo
+from sciluigi.dependencies import DependencyHelpers
 
 from sciluigi import parameter
 from sciluigi.parameter import Parameter
@@ -43,3 +43,11 @@ from sciluigi.util import timestamp
 from sciluigi.util import timepath
 from sciluigi.util import recordfile_to_dict
 from sciluigi.util import dict_to_recordfile
+
+__all__ = ['interface', 'run', 'run_local', 'LOGFMT_STREAM', 'LOGFMT_LUIGI',
+        'LOGFMT_SCILUIGI', 'DATEFMT', 'audit', 'AuditTrailHelpers',
+        'dependencies', 'TargetInfo', 'S3TargetInfo', 'DependencyHelpers',
+        'parameter', 'Parameter', 'slurm', 'SlurmInfo', 'SlurmTask',
+        'SlurmHelpers', 'RUNMODE_LOCAL', 'RUNMODE_HPC', 'RUNMODE_MPI', 'task',
+        'new_task', 'Task', 'ExternalTask', 'WorkflowTask', 'util',
+        'timestamp', 'timepath', 'recordfile_to_dict', 'dict_to_recordfile']
